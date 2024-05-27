8a1e44721f714762dd6bd6443a47b144fbbd0e36
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2024-05-27 11:48:06 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ Config.VisuallyDamageCars = true   -- true == damage car on spawn | false == no 
 Config.SharedGarages = false       -- true == take any car from any garage | false == only take car from garage stored in
 Config.ClassSystem = false         -- true == restrict vehicles by class | false == any vehicle class in any garage
 Config.FuelResource = 'LegacyFuel' -- supports any that has a GetFuel() and SetFuel() export
-Config.Warp = true                 -- true == warp player into vehicle | false == vehicle spawns without warping
+Config.Warp = false                -- true == warp player into vehicle | false == vehicle spawns without warping
 
 -- https://docs.fivem.net/natives/?_0x29439776AAA00A62
 Config.VehicleClass = {
